6a3e386c0a88f9842d4a365518b8a3873cebc2db
Md Aqil <aqil@wipl.co.in>
2020-12-13 02:03:14 +0530

    added

--- assets/js/main.js
+++ assets/js/main.js
@@ -81,47 +81,28 @@ var chart = new Chart(ctx, {
 });
 
 var swiperMobile = new Swiper('.swiper-container.swiper-full-mobile', {
-    slidesPerView: 5,
+    slidesPerView: 4,
     spaceBetween: 20,
     slideToClickedSlide:true,
     centeredSlides:true,
    pagination: {
       el: '.swiper-pagination',
       clickable: true,
-
     },
-
     loop:true,
     //   autoplay: {
     //     delay: 100000,
     //   },
 
-
       keyboard: {
       enabled: true,
       onlyInViewport: true,
     },
-
-
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
 
-
-     breakpoints: {
-          
-          640: {
-            freemode:true,
-            slidesPerView: 3,
-            spaceBetween: 20,
-          },
-          320: {
-            freemode:true,
-            slidesPerView: 3,
-            spaceBetween: 20,
-          }
-    }
 
   });
   
